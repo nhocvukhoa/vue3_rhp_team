f7867834f05874d80936a563e7fa66c9cc1fc7ba
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-01-31 20:39:49 +0700

    conditional rendering

--- chapter_1/main.js
+++ chapter_1/main.js
@@ -1,16 +1,23 @@
 const app = Vue.createApp({
+    template: `<h1>Hello {{ teamName }} and {{ !isHello ? firstName + lastName : '' }}</h1>
+        <button @click="onChangeName">Change name</button>
+        <div class="modal" v-if="isShowModal">
+            <h1>Modal content</h1>
+        </div>`,
     data() {
         return {
             teamName: 'RHP Team',
             firstName: 'Anh',
             lastName: 'Khoa',
             isHello: false,
+            isShowModal: false,
         }
     },
     methods: {
         onChangeName() {
             this.firstName = 'Ngọc';
             this.lastName = 'Anh';
+            this.isShowModal = !this.isShowModal;
         }
     }
 });
