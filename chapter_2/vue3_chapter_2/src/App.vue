c1b01bab7a5f6d010c8a8f183a17fe8a57704995
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-02-13 10:43:53 +0700

    use props

--- chapter_2/vue3_chapter_2/src/App.vue
+++ chapter_2/vue3_chapter_2/src/App.vue
@@ -1,37 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld ref="hello" msg="Welcome to Your Vue.js App" />
-  <header-page ref="header" />
-  <button @click="onChangeMe">On change me</button>
+  <h1>Hello Vue Cli</h1>
+  <base-page
+    title="This is new title"
+    content="This is content"
+    theme="contact"
+  />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import HeaderPage from "./components/HeaderPage.vue";
+import BasePage from "./components/BaseModal.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
-    HeaderPage,
-  },
-  methods: {
-    onChangeMe() {
-      console.log("running here");
-      console.log(this.$refs.hello.onTesting());
-      console.log(this.$refs.header);
-    },
+    BasePage,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
